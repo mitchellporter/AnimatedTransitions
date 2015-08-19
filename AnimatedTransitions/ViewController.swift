dfeb49eca00e4371174dcee56dea4b2437e4564d
Mitchell Porter <mitchellporter@gmail.com>
2015-08-19 16:39:32 -0700

    Basic animated transition is now working properly. The animation is visually different though fromm the gif shown on the page

--- AnimatedTransitions/ViewController.swift
+++ AnimatedTransitions/ViewController.swift
@@ -9,19 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // add this right above your viewDidLoad function...
+    let transitionManager = TransitionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     
     @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // this gets a reference to the screen that we're about to transition to
+        let toViewController = segue.destinationViewController as! UIViewController
+        
+        // instead of using the default transition animation, we'll ask
+        // the segue to use our custom TransitionManager object to manage the transition animation
+        toViewController.transitioningDelegate = self.transitionManager
         
     }
 
