65cf84ab1058d3a1e92015f3529ca22ed6d6ec0a
Mitchell Porter <mitchellporter@gmail.com>
2015-08-19 16:27:31 -0700

    Initial commit. This is part 1 of mathewsanders.com animated transitions tutorials. Setup segues for both view controllers on the Storyboard including an exit segue for the second one.

---
+++ AnimatedTransitions/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AnimatedTransitions
+//
+//  Created by Mitchell Porter on 8/19/15.
+//  Copyright (c) 2015 Mentor Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func unwindToViewController (sender: UIStoryboardSegue){
+        
+    }
+
+}
+
