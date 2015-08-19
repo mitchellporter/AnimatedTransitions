0be037d90c69c96543d1681a1622f1f043307c73
Mitchell Porter <mitchellporter@gmail.com>
2015-08-19 16:32:40 -0700

    created TransitionManager class

---
+++ AnimatedTransitions/TransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionManager.swift
+//  AnimatedTransitions
+//
+//  Created by Mitchell Porter on 8/19/15.
+//  Copyright (c) 2015 Mentor Ventures. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject {
+   
+}
